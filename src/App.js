ea96dd7db686ee074d8f546fc3f7f9f242aab19d
Valentin Vladimirov <valentinvladimirov@abv.bg>
2022-12-15 20:31:43 +0200

    task-75 done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,51 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [note, setNote] = useState("");
+
+  function handleChange(event) {
+    setNote(event.target.value);
+  }
+
+  function saveNote() {
+    localStorage.setItem("note", note);
+  }
+
+  function restoreNote() {
+    setNote("");
+    localStorage.removeItem("note");
+  }
+
+  useEffect(() => {
+    const savedNote = localStorage.getItem("note");
+    if (savedNote) {
+      setNote(savedNote);
+    }
+  }, []);
+
   return (
     <div className="App">
       <div className="box">
         <div className="field">
           <div className="control">
-            <textarea className="textarea is-large" placeholder="Notes..." />
+            <textarea
+              className="textarea is-large"
+              placeholder="Notes..."
+              value={note}
+              onChange={handleChange}
+            />
           </div>
         </div>
-        <button className="button is-large is-primary is-active">Save</button>
-        <button className="button is-large">Clear</button>
+        <button
+          className="button is-large is-primary is-active"
+          onClick={saveNote}
+        >
+          Save
+        </button>
+        <button className="button is-large" onClick={restoreNote}>
+          Clear
+        </button>
       </div>
     </div>
   );
